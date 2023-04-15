321bb76bcb62ec02817e6dbf4720a2db1aadbc10
Basinger <fel_simon@mail.ru>
2023-04-15 19:02:01 +0300

    Added Task001_TwoDimArray

---
+++ Task001_TwoDimArray/Program.cs
@@ -0,0 +1,45 @@
+﻿//  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int SizeOfArray(string i)
+{
+    System.Console.Write($"Enter {i}: ");
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+double CreateArrayRange(string i)
+{
+    System.Console.Write($"Enter {i}: ");
+    double x = double.Parse(Console.ReadLine());
+    return x;
+}
+
+double[,] CreateArray()
+{
+    double[,] Array = new double[SizeOfArray("m"), SizeOfArray("n")];
+    double a = CreateArrayRange("lower limit of your array");
+    double b = CreateArrayRange("upper limit of your array");
+    
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = Math.Round(new Random().NextDouble() * (b - a) + a, 2);
+        }
+    }
+    return Array;
+}
+
+void PrintArray(double[,] Array)
+{
+     for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            System.Console.Write(Array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+PrintArray(CreateArray());
